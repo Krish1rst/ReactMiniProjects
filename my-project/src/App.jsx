8d91554c1d9961becf3f25c81e9b9d1e6844bc69
krishna <Krishnayadav48065@gmail.com>
2024-02-25 19:27:36 +0530

    Toastify practice

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,14 +2,30 @@ import React,{useState} from 'react'
 import StarRating from './StarRating/index'
 import Clock from './Clock/index'
 import FetchData from './FetchData/FetchData'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
-
+  const notify = () => toast.success("Wow so easy!");
   return (
     <>
-   
+       <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer
+position="top-center"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+
+/>
+    </div>
       {/* <StarRating/> */}
       {/* <Clock/> */}
         {/* <FetchData/> */}
--- my-project/src/FetchData/FetchData.jsx
+++ my-project/src/FetchData/FetchData.jsx
@@ -2,19 +2,29 @@ import React from 'react'
 import useFetch from '../utils/useFetch'
 
 function FetchData() {
-const url='https://jsonplaceholder.typicode.com/posts';
+const url='https://api.escuelajs.co/api/v1/products';
 const {data,error,isLoading}=useFetch(url)
+
+// const [img1,img2,img3]=images; 
 console.log(data)
+
 if (isLoading) return <div>Loading...</div>;
 if (error) return <div>Error: {error.message}</div>;
   return (
-    <div className='flex flex-col p-8  gap-8'>  {data && data.map(post => (
-        <div key={post.id} className='flex flex-col gap-2 bg-slate-300 rounded-xl p-4'>
-          <h2 className='text-md text-gray-700 font-semibold line-clamp-2 '>TITLE :  {post.title}</h2>
-          <p className='text-sm text-gray-600 underline '>{post.body}</p>
-        </div>
-      ))}
+    <div className='flex flex-col p-8 gap-8'> 
+  {data && data.map(post => (
+    <div key={post.id} className="flex flex-col gap-4">
+      <h2 className="text-xl font-bold">{post.title}</h2>
+      <h2 className="text-xl font-bold">{post.category?.name}</h2>
+      <p className="text-gray-500">{post.description}</p>
+      <div className="flex gap-4">
+        {post?.images.map(imageUrl => (
+          <img key={imageUrl} src={imageUrl} alt="Product" className="w-32 h-32 object-cover" />
+        ))}
       </div>
+    </div>
+  ))}
+</div>
   )
 }
 
