340bb80714165b4c33d98803eb88bcc29b777802
krishna <Krishnayadav48065@gmail.com>
2024-02-26 14:24:39 +0530

    Redux cart setup mini project done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,29 +2,18 @@ import React,{useState} from 'react'
 import StarRating from './StarRating/index'
 import Clock from './Clock/index'
 import FetchData from './FetchData/FetchData'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+
 
 function App() {
 
-  const notify = () => toast.success("Wow so easy!");
+  const notify = () => toast.success("Success");
   return (
     <>
        <div>
       <button onClick={notify}>Notify!</button>
-      <ToastContainer
-position="top-center"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
+ 
 
-/>
     </div>
       {/* <StarRating/> */}
       {/* <Clock/> */}
