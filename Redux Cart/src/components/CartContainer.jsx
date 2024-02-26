f69f719e86e4d5292f57a13283b446f2a5fdf954
krishna <Krishnayadav48065@gmail.com>
2024-02-26 16:33:29 +0530

    UI components done

--- Redux Cart/src/components/CartContainer.jsx
+++ Redux Cart/src/components/CartContainer.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
+import CartItem from './CartItem'
 import { useSelector } from 'react-redux'
 function CartContainer() {
 const {cartItem,amount,total} =useSelector((store)=>store.cart)
         
         
+if (amount < 1) {
   return (
-    <div>CartContainer</div>
-  )
+    <section className='cart'>
+      {/* cart header */}
+      <header>
+        <h2>your bag</h2>
+        <h4 className='empty-cart'>is currently empty</h4>
+      </header>
+    </section>
+  );
 }
+return (
+  <section className='cart'>
+    {/* cart header */}
+    <header>
+      <h2>your bag</h2>
+    </header>
+    {/* cart items */}
+    <div>
+      {cartItem.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+    </div>
+    {/* cart footer */}
+    <footer>
+      <hr />
+      <div className='cart-total'>
+        <h4>
+          total <span>${total}</span>
+        </h4>
+      </div>
+      <button className='btn clear-btn'>clear cart</button>
+    </footer>
+  </section>
+);
+};
+
 
 export default CartContainer
--- Redux Cart/src/components/CartItem.jsx
+++ Redux Cart/src/components/CartItem.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { ChevronDown, ChevronUp } from '../icons';
 
-function CartItem() {
+const CartItem = ({ id, img, title, price, amount }) => {
   return (
-    <div>CartItem</div>
-  )
-}
+    <article className='cart-item'>
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className='item-price'>${price}</h4>
+        {/* remove button */}
+        <button className='remove-btn'>remove</button>
+      </div>
+      <div>
+        {/* increase amount */}
+        <button className='amount-btn'>
+          <ChevronUp />
+        </button>
+        {/* amount */}
+        <p className='amount'>{amount}</p>
+        {/* decrease amount */}
+        <button className='amount-btn'>
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
+};
 
-export default CartItem
+export default CartItem;
